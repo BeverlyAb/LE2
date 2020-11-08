e781948cae36be50d86d75d02dc7e6a203d0e054
babadine <bev.abadines@gmail.com>
2020-11-07 18:35:28 -0800

    added checkers

--- LE2/ContentView.swift
+++ LE2/ContentView.swift
@@ -19,9 +19,26 @@ struct ContentView: View {
     var recognitionTask: SFSpeechRecognitionTask?
 
     func recordAndRecognizeSpeech(){
-        let node = audioEngine.inputNode 
+        let node = audioEngine.inputNode
         let recordingFormat = node.outputFormat(forBus:0)
         node.installTap(onBus: 0, bufferSize:1024, format:recordingFormat) { buffer, _ in self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        //checkers
+        do{
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            return
+        }
+        
+        if !myRecognizer.isAvailable {
+            return
         }
     }
     
