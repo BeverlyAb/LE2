842aef9617c7731ded1284b431404696397ca8d2
babadine <bev.abadines@gmail.com>
2020-11-08 02:15:43 -0800

    Adjusted visual display

--- LE2/Visual/BarView.swift
+++ LE2/Visual/BarView.swift
@@ -15,8 +15,8 @@ struct BarView: View{
     var body: some View{
         ZStack{
             RoundedRectangle(cornerRadius: 20)
-                .fill(LinearGradient(gradient:Gradient(colors:[.blue,.purple]),startPoint:.top,endPoint:.bottom))
-            .frame(width: (UIScreen.main.bounds.width - CGFloat(numberOfSamples))*10/CGFloat(numberOfSamples), height: value)
+                .fill(LinearGradient(gradient:Gradient(colors:[.yellow,.blue]),startPoint:.top,endPoint:.bottom))
+            .frame(width: (UIScreen.main.bounds.width - CGFloat(numberOfSamples)*9)/CGFloat(numberOfSamples), height: value*5)
         }
     }
 }
